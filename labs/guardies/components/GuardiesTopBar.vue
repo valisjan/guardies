0c83c7eba9be5dc13d4f1d7d69352a18b70e5304
Jose Antonio <jaerustes@gmail.com>
2026-09-16 15:48:44 +0200

    Link guardies to Pantalles custom domain

--- labs/guardies/components/GuardiesTopBar.vue
+++ labs/guardies/components/GuardiesTopBar.vue
@@ -42,7 +42,7 @@ function toggleTheme() {
         <a href="https://quota.iessureda.com/">Quota</a>
         <a v-if="isAdmin" :class="{ active: !teacherView }" :href="guardiesHref" :aria-current="!teacherView ? 'page' : undefined">Guàrdies</a>
         <a :class="{ active: teacherView }" :href="professoratHref" :aria-current="teacherView ? 'page' : undefined">Professorat</a>
-        <a v-if="isAdmin" href="https://quota.iessureda.com/labs/pantalles/?gestio=1&pantalla=sala-professorat">Pantalles</a>
+        <a v-if="isAdmin" href="https://pantalles.iessureda.com/?gestio=1&pantalla=sala-professorat">Pantalles</a>
       </div>
       <button id="theme-toggle" type="button" class="theme-toggle" aria-label="Canvia el tema" @click="toggleTheme">
         <span class="theme-icon" aria-hidden="true">◐</span>
